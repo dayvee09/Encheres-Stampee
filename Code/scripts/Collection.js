93f49fc59c60f25c1d60c715cb3b13a2a455578c
David Deschenes <davids09@hotmail.com>
2022-04-21 03:00:16 -0400

    Version remise

--- Code/scripts/Collection.js
+++ Code/scripts/Collection.js
@@ -77,6 +77,7 @@ export default class Collection {
     } while (diffDays < 0 || diffDays > 30);
     let miseCourante = nouveauTimbre.mise;
     let description = nouveauTimbre.lorem(25);
+    let textAdditionnel = nouveauTimbre.lorem(5);
     // Génère ici une DOMString qui contient le lien relatif associé à l'image d'un timbre
     let timbreImg = `
         <img src="./assets/img/${timbreNom}.jpg" class="img--timbre" alt="${timbreNom}"/>
@@ -94,9 +95,9 @@ export default class Collection {
             </div>
                 
               <div class="timbre__info">
-                <h1 class="titre--timbre">${timbreNom} - Lot de timbres #${
+                <h1 class="titre--timbre">${timbreNom} - Lot #${
       i + 1
-    }</h1>
+    } - ${textAdditionnel}</h1>
               </div>
             <div class="catalogue__timbre--txt">
               <div class="enchere__info">
